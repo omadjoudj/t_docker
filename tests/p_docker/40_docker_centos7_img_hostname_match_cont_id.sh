27b6ca6abf3d915e41cd3d79e1ce30fa2f42ed01
Athmane Madjoudj <athmane@fedoraproject.org>
2014-11-07 19:21:46 +0100

    Fix hostname test on c7

--- tests/p_docker/40_docker_centos7_img_hostname_match_cont_id.sh
+++ tests/p_docker/40_docker_centos7_img_hostname_match_cont_id.sh
@@ -4,11 +4,10 @@
 t_Log "Running $0 - Check if hostname inside CentOS 7 image matches container id"
 
 
-hostname_output="`docker run centos:centos7 hostname`"
+hostname_output="`docker run centos:centos7 uname -n`"
 
-#docker ps -a | grep -q "$hostname_output"
+docker ps -a | grep -q "$hostname_output"
 
-t_Log "Pass: currently CentOS 7 image does not have hostname command"
 
 t_CheckExitStatus $?
 
